f32153222c0376a67198106886f82b176f7bc86b
Paul Breslin <paul.breslin@deliveroo.co.uk>
2019-04-02 22:02:11 +0100

    Shows not found message

--- contentscript/src/components/WordCard/index.tsx
+++ contentscript/src/components/WordCard/index.tsx
@@ -4,33 +4,36 @@ import DefinitionList from '../DefinitionList/index.tsx';
 import * as styles from './styles.css';
 
 interface WordCardProps {
-    word: string;
-    onCloseClick: () => void;
-    isDefinitionLoading: boolean;
+  word: string;
+  onCloseClick: () => void;
+  isDefinitionLoading: boolean;
 }
 
 const WordCard = ({
-    word,
-    definitionList,
-    isDefinitionLoading,
-    onCloseClick
+  word,
+  definitionList: fullDefinitionList,
+  isDefinitionLoading,
+  onCloseClick
 }: WordCardProps) => {
-    return (
-        <div className={styles.wordCard}>
-            <div onClick={onCloseClick} className={styles.removeIcon}>
-                <div>&times;</div>
-            </div>
-            <span className={styles.wordCardTitle}>{word}</span>
-            <span />
-            {isDefinitionLoading ? (
-                <div className={styles.loading}>
-                    Searching for definition...
-                </div>
-            ) : (
-                <DefinitionList definitionList={definitionList} />
-            )}
-        </div>
-    );
+  const showNotFound =
+    Array.isArray(fullDefinitionList) &&
+    fullDefinitionList.every(({ definitionList }) => !definitionList);
+
+  return (
+    <div className={styles.wordCard}>
+      <div onClick={onCloseClick} className={styles.removeIcon}>
+        <div>&times;</div>
+      </div>
+      <span className={styles.wordCardTitle}>{word}</span>
+      {showNotFound && (
+        <div className={styles.notFound}>No definition found</div>
+      )}
+      {isDefinitionLoading && (
+        <div className={styles.loading}>Searching for definition...</div>
+      )}
+      <DefinitionList definitionList={fullDefinitionList} />
+    </div>
+  );
 };
 
 export default WordCard;
